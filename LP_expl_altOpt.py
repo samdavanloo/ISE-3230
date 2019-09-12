ea41cfe3d2c1c56cf1d40cd918761ed2b38ee3fd
Sam Davanloo <davanloo.1@osu.edu>
2019-09-12 12:27:13 -0400

    adding PULP codes

--- LP_expl_altOpt.py
+++ LP_expl_altOpt.py
@@ -5,7 +5,7 @@ Created on Thu Sep  5 10:59:11 2019
 
 @author: Sam
 """
-
+#%% Implementation in CVXOPT
 from cvxopt.modeling import variable,op
 x_1 = variable()
 x_2 = variable()
@@ -23,3 +23,22 @@ print(c1.multiplier.value)
 print(c2.multiplier.value)
 print(c3.multiplier.value)
 print(c4.multiplier.value)
+
+
+#%% Implementation in PULP
+from pulp import *
+pulp.pulpTestAll() # testing pulp
+
+prob = LpProblem("expl_alt_optimal",LpMaximize) # create an LP maximization problem
+x_1 = LpVariable("x_1",lowBound=0) 
+x_2 = LpVariable("x_2",lowBound=0)
+prob += 6*x_1 + 3*x_2 # objective function
+prob += 2*x_1+x_2 <= 16 
+prob += x_1 + x_2 <= 10
+prob # display the LP problem
+status = prob.solve() # solve with default solver
+LpStatus[status]   # PULP also doesn NOT report existence of alternative optimal 
+
+for v in prob.variables():
+    print(v.name, "=", v.varValue)
+print("Total Cost = ", value(prob.objective))
--- LP_expl_unbounded.py
+++ LP_expl_unbounded.py
@@ -5,7 +5,7 @@ Created on Thu Sep  5 10:47:40 2019
 
 @author: Sam
 """
-
+#%% Implementation in CVXOPT
 from cvxopt.modeling import variable,op
 x_1 = variable()
 x_2 = variable()
@@ -20,3 +20,18 @@ lp1.status
 print(x_1.value)
 print(x_2.value)
 print(lp1.objective.value())
+
+#%% Implementation in PULP
+from pulp import *
+pulp.pulpTestAll() # testing pulp
+
+prob = LpProblem("expl_unbounded_optimal",LpMaximize) # create an LP maximization problem
+x_1 = LpVariable("x_1",lowBound=0) 
+x_2 = LpVariable("x_2",lowBound=0)
+prob += x_1 + 3*x_2 # objective function
+prob += x_1-2*x_2 <= 4 
+prob += -x_1 + x_2 <= 3
+prob += x_2 >= 2
+prob # display the LP problem
+status = prob.solve() # solve with default solver
+LpStatus[status]   # PULP explicitly reports unboundedness
