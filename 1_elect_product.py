c10700a4a436e5176d1347f3536a733d1a30958e
Sam Davanloo <davanloo.1@osu.edu>
2020-08-27 10:23:22 -0400

    Rewritting everything in cvxpy

---
+++ 1_elect_product.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+@author: Sam Davanloo
+
+"""
+
+import cvxpy as cp
+
+x = cp.Variable(2, nonneg  = True) # vector variable
+
+constraints = []
+constraints.append((2/3)*x[0]+x[1]<=18)
+constraints.append(2*x[0]+x[1]>=8)
+constraints.append(x[0]<=12)
+constraints.append(x[1]<=16)
+
+obj_func=x[0]+x[1]
+
+problem = cp.Problem(cp.Maximize(obj_func), constraints)
+
+problem.solve(solver=cp.CVXOPT,verbose = True) #verbose parameter determines showing/not showing the output
+
+print("obj_func =")
+print(obj_func.value)
+print("x =")
+print(x.value)
