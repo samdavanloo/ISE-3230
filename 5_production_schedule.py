b737f73a62b243549068db8304541b1f4227573d
Sam Davanloo <davanloo.1@osu.edu>
2020-09-03 12:41:54 -0400

    New files added

---
+++ 5_production_schedule.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 12:10:29 2020
+
+CVXPY code for the LP production schedule example.
+
+@author: Sam
+"""
+
+import cvxpy as cp
+import numpy as np
+
+
+x = cp.Variable(4, nonneg  = True) # vector variable
+y = cp.Variable(3, nonneg  = True) # vector variable
+w = cp.Variable(3, nonneg  = True) # vector variable
+
+
+obj_func=12*x[0]+12*x[1]+14*x[2]+14*x[3]+16*y[0]+16*y[1]+3*(w[0]+w[1]+w[2])
+
+
+constraints = []
+constraints.append(x[0]-w[0]==400)
+constraints.append(x[1]+y[0]+w[0]-w[1]==750)
+constraints.append(x[2]+y[1]+w[1]-w[2]==950)
+constraints.append(x[3]+w[2]==700)  # to fix infeasibility change 900 to 700
+constraints.append(x[0]<=800)
+constraints.append(x[1]<=800)
+constraints.append(x[2]<=800)
+constraints.append(x[3]<=800)
+constraints.append(y[0]<=200)
+constraints.append(y[1]<=200)
+constraints.append(w[0]<=50)
+constraints.append(w[1]<=50)
+constraints.append(w[2]<=50)
+
+
+
+problem = cp.Problem(cp.Minimize(obj_func), constraints)
+
+problem.solve(solver=cp.CVXOPT,verbose = True) #verbose parameter determines showing/not showing the output
+
+print("obj_func =")
+print(obj_func.value)
+print("x =")
+print(x.value)
+print("y =")
+print(y.value)
+print("w =")
+print(w.value)
---
+++ 6_investment.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 12:23:55 2020
+
+CVXPY code for the LP investment example.
+
+@author: Sam
+"""
+
+import cvxpy as cp
+import numpy as np
+
+x = cp.Variable(5, nonneg  = True) # vector variable
+s = cp.Variable(3, nonneg  = True) # vector variable
+
+obj_func=x[1]+1.9*x[3]+1.5*x[4]+1.08*s[2]
+
+constraints = []
+constraints.append(x[0]+x[2]+x[3]+s[0]==100000)
+constraints.append(0.5*x[0]-x[1]+1.2*x[2]+1.08*s[0]-s[1]==0)
+constraints.append(x[0]+0.5*x[1]-x[4]+1.08*s[1]-s[2]==0)
+constraints.append(x[0]<=75000)
+constraints.append(x[1]<=75000)
+constraints.append(x[2]<=75000)
+constraints.append(x[3]<=75000)
+constraints.append(x[4]<=75000)
+
+
+problem = cp.Problem(cp.Maximize(obj_func), constraints)
+
+problem.solve(solver=cp.CVXOPT,verbose = True)
+
+print("obj_func =")
+print(obj_func.value)
+print("x =")
+print(x.value)
+print("y =")
+print(s.value)
